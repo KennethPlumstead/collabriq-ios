8de89875a202fbbfb600dfc6b2cd3cc73333906a
Kenneth Plumstead <kennethplumstead@icloud.com>
2025-09-26 10:27:44 -0400

    chore: scaffold Collabriq Xcode project

---
+++ Collabriq/CollabriqApp.swift
@@ -0,0 +1,17 @@
+//
+//  CollabriqApp.swift
+//  Collabriq
+//
+//  Created by Kenneth Plumstead on 2025-09-26.
+//
+
+import SwiftUI
+
+@main
+struct CollabriqApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
